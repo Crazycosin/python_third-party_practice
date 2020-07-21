57fe46c57313b0de51319886750fa13adffb99a3
wangwenjun <837122529@qq.com>
2020-07-21 22:48:35 +0800

    image_solution - face_recognition - finish

--- image_solution/face_recognition_test.py
+++ image_solution/face_recognition_test.py
@@ -137,8 +137,53 @@ def face_recognition_test_2():
                 print('这个人是:' + labels[i])
 
 
+def face_recognition_test_3():
+    # 笔记本电脑的摄像头是 0，外接摄像头是 1
+    video_capture = cv2.VideoCapture(0)
+    wi_filename = PROJECT_PATH + '/res/willian.jpg'
+    ma_filename = PROJECT_PATH + '/res/maria.jpg'
+    obama_img = face_recognition.load_image_file(wi_filename)
+    obama_face_encoding = face_recognition.face_encodings(obama_img)[0]
+
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
+
+    while True:
+        ret, frame = video_capture.read()
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        if process_this_frame:
+            face_locations = face_recognition.face_locations(small_frame)
+            face_encodings = face_recognition.face_encodings(small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                    match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+                    if match[0]:
+                        name = 'willian'
+                    else:
+                        name = 'unknown'
+                    face_names.append(name)
+        process_this_frame = not process_this_frame
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255),  2)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), 2)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left+6, bottom-6), font, 1.0, (255, 255, 255), 1)
+        cv2.imshow('Video',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == "__main__":
     # face_recognition_test()
     # check_face_location()
     # face_recognition_test_1()
-    face_recognition_test_2()
+    # face_recognition_test_2()
+    face_recognition_test_3()
